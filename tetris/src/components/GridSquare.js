75139b6d7d89bb3dba457bec062cd534f8d30df6
mattyImry <matteo.imeri@gmail.com>
2023-06-15 08:45:09 +0100

    refactor css to Gridsquare module.css

--- tetris/src/components/GridSquare.js
+++ tetris/src/components/GridSquare.js
@@ -1,10 +1,10 @@
 // this component rapresent a grid square with color
 
-import classes from "./GridSquare.module.css"
+import classes from "./GridSquare.module.css";
 
-const GridSquare = (props) =>{
-    const classGrid = `grid-square color-${props.color}`
-    return <div className={classGrid}></div>
-}
+const GridSquare = (props) => {
+  const cssColor = `color_${props.color}`;
+  return <div className={`${classes.gridsquare} ${classes[cssColor]}`}></div>;
+};
 
-export default GridSquare
+export default GridSquare;
