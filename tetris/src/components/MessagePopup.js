67b21eb5a9601d41d9039274278e3407f60f69d1
mattyImry <matteo.imeri@gmail.com>
2023-06-16 08:42:33 +0100

    add MessagePopup.js and styles

---
+++ tetris/src/components/MessagePopup.js
@@ -0,0 +1,12 @@
+import classes from'./MessagePopup.module.css'
+
+const MessagePopup = props =>{
+    return (
+        <div className={classes.message_popup}>
+          <h1>Message Title</h1>
+          <p>Message info...</p>
+        </div>
+      )
+}
+
+export default MessagePopup;
