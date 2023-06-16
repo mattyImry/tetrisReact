4c23c7489778bde1deaab15d91178632af1b49d2
mattyImry <matteo.imeri@gmail.com>
2023-06-16 08:36:14 +0100

    add Controls.js and styles

---
+++ tetris/src/components/Controls.js
@@ -0,0 +1,25 @@
+import classes from "./Controls.module.css";
+
+const Controls = (props) => {
+  return (
+    <div className={classes.controls}>
+      <button className={classes.control_button} onClick={(e) => {}}>
+        Left
+      </button>
+
+      <button className={classes.control_button} onClick={(e) => {}}>
+        Right
+      </button>
+
+      <button className={classes.control_button} onClick={(e) => {}}>
+        Rotate
+      </button>
+
+      <button className={classes.control_button} onClick={(e) => {}}>
+        Down
+      </button>
+    </div>
+  );
+};
+
+export default Controls;
