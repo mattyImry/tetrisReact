0f491acb2ca8c999dee7073eba3dc9146600dedc
mattyImry <matteo.imeri@gmail.com>
2023-06-16 08:18:08 +0100

    style App.js with module.css

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
+import classes from "./App.module.css";
 import GridBoard from "./components/GridBoard";
 import NextBlock from "./components/NextBlock";
 import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Tetris</h1>
+    <div className={classes.App}>
+      <header className={classes.App_header}>
+        <h1 className={classes.App_title}>Tetris</h1>
       </header>
       <GridBoard />
       <NextBlock />
