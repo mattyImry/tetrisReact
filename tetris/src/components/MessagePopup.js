e9d23e8ff6d691153bc3ffc3f60f1fde9ad732e3
mattyImry <matteo.imeri@gmail.com>
2023-06-22 09:19:46 +0100

    messagePopup connected to state

--- tetris/src/components/MessagePopup.js
+++ tetris/src/components/MessagePopup.js
@@ -1,14 +1,27 @@
 // message disply maybe use as overlay??
 
-import classes from'./MessagePopup.module.css'
+import classes from "./MessagePopup.module.css";
+import { useSelector } from "react-redux";
 
-const MessagePopup = props =>{
-    return (
-        <div className={classes.message_popup}>
-          <h1>Message Title</h1>
-          <p>Message info...</p>
-        </div>
-      )
-}
+const MessagePopup = (props) => {
+  const isRunning = useSelector((state) => state.game.isRunning);
+  const gameOver = useSelector((state) => state.game.gameOver);
+  let message = "";
+  let isHidden = "hidden";
+
+  if (gameOver) {
+    message = "Game Over";
+    isHidden = "";
+  } else if (!isRunning) {
+    message = "Paused";
+    isHidden = "";
+  }
+
+  return (
+    <div className={`${classes.message_popup} ${classes[isHidden]}`}>
+      <h1>{message}</h1>
+    </div>
+  );
+};
 
 export default MessagePopup;
