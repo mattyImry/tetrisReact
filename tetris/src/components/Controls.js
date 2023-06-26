fab375672ecba69d41e9e3dd6e10c68fb61617dc
mattyImry <matteo.imeri@gmail.com>
2023-06-26 10:48:49 +0100

    add key press to controls

--- tetris/src/components/Controls.js
+++ tetris/src/components/Controls.js
@@ -1,11 +1,43 @@
 //setting control for the game
 import { useSelector, useDispatch } from "react-redux";
+
 import { moveDown, moveLeft, moveRight, rotate } from "../actions";
 import classes from "./Controls.module.css";
+import { useEffect } from "react";
 
 const Controls = (props) => {
   const dispatch = useDispatch();
   const isRunning = useSelector((state) => state.isRunning);
+
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.keyCode === 37) {
+        dispatch(moveLeft());
+      }
+
+      if (e.keyCode === 39) {
+        dispatch(moveRight());
+      }
+
+      if (e.keyCode === 40) {
+        dispatch(moveDown());
+      }
+
+      if (e.keyCode === 32) {
+        dispatch(rotate());
+      }
+
+      // console.log(e.keyCode);
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Don't forget to clean up
+    return function cleanup() {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
   return (
     <div className={classes.controls}>
       <button
